0e29cb772740a8601d1b83b017097126bdad5067
M Mansoor Ali <muhammadmansourali@gmail.com>
2015-06-24 01:30:15 +0500

    Added Question Controller and ckeditor

---
+++ MezoExperts/Controllers/QuestionController.cs
@@ -0,0 +1,25 @@
+﻿using MezoExperts.Filters;
+using MezoExperts.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MezoExperts.Controllers
+{
+    [Authorize]
+    [InitializeSimpleMembership]
+    public class QuestionController : Controller
+    {
+        //
+        // GET: /Question/
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Post(string q="")
+        {
+            return View(new HomeModel());
+        }
+
+    }
+}
