ac5fc2397e43e12b1d6caea0f4db2a910c5eae8d
M Mansoor Ali <muhammadmansourali@gmail.com>
2015-06-25 06:54:16 +0500

    Reverted all changes at my end

---
+++ MezoExperts/Controllers/QuestionController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MezoExperts.Models;
+
+namespace MezoExperts.Controllers
+{
+    public class QuestionController : Controller
+    {
+        private DBEntities db = new DBEntities();
+
+        //
+        // GET: /Question/
+
+        public ActionResult Index()
+        {
+            var questions = db.Questions.Include(q => q.Category);
+            return View(questions.ToList());
+        }
+
+        //
+        // GET: /Question/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Question question = db.Questions.Find(id);
+            if (question == null)
+            {
+                return HttpNotFound();
+            }
+            return View(question);
+        }
+
+        //
+        // GET: /Question/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "Id");
+            return View();
+        }
+
+        //
+        // POST: /Question/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Question question)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Questions.Add(question);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "Id", question.CategoryId);
+            return View(question);
+        }
+
+        //
+        // GET: /Question/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Question question = db.Questions.Find(id);
+            if (question == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "Id", question.CategoryId);
+            return View(question);
+        }
+
+        //
+        // POST: /Question/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Question question)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(question).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "Id", question.CategoryId);
+            return View(question);
+        }
+
+        //
+        // GET: /Question/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Question question = db.Questions.Find(id);
+            if (question == null)
+            {
+                return HttpNotFound();
+            }
+            return View(question);
+        }
+
+        //
+        // POST: /Question/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Question question = db.Questions.Find(id);
+            db.Questions.Remove(question);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
